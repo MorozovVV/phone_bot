3745afe0a781eed793ee63546af5eac1720d663d
MorozovVV <33996016+MorozovVV@users.noreply.github.com>
2020-02-07 12:28:50 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,5 +6,5 @@ bot = telebot.TeleBot('%1086391238:AAE6ZwO5DiiwwYkF69io1u1o16RJ8ZrvlQc%')
 def get_text_messages(message):
     global phonenomber;
     phonenomber = message.text;
-    bot.send_message(message.from_user.id, 'htts://api.whatsapp.com/send?phone='+phonenomber)
+    bot.send_message(message.from_user.id, 'htts://api.whatsapp.com/send?phone=' + phonenomber)
 bot.polling(none_stop=True, interval=0)
