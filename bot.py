628e84c1c00a29a84abda2bc041e8cf0dc91a760
MorozovVV <33996016+MorozovVV@users.noreply.github.com>
2020-02-07 13:43:12 +1000

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,11 +2,11 @@ import os
 import telebot
 from telebot import types
 TOKEN = os.environ["TOKEN"]
-phonenomber = '';
+
 bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(content_typs=['text'])
 def get_text_messages(message):
-    global phonenomber;
+    phonenomber = '';
     phonenomber = message.text;
     bot.send_message(message.from_user.id, 'htts://api.whatsapp.com/send?phone=' + phonenomber)
 bot.polling(none_stop=True, interval=0)
